afcf248226d21e19fb053132f36da19772f61740
Marcos Brendon De Paula <166177842+DravenOcarrasco@users.noreply.github.com>
2024-10-04 23:52:45 -0300

    updated extension structure to version 2.7.0
    - Refactored the default functions to be part of the quick context creation process.
    - Default WebSocket, storage, and menu handling functions are now loaded from an external file.
    - Simplified module setup by automatically loading core functions during context initialization.
    - Improved maintainability and modularity by separating core logic into a separate file.

--- client.js
+++ client.js
@@ -1,96 +1,99 @@
 (async function () {
     /**
-     * Function to create the context for the module.
-     * @returns {Promise<object>} - The context object containing module details and methods.
-     */
-    async function MakeContext() {
-        const MODULE_NAME = "CONNECTION-INFO";
-        const SOCKET = io(`http://${window.WSACTION.config.ip}:${window.WSACTION.config.port}/`, { 
-            secure: false,
-            reconnection: true,  // Ativa a reconexão automática
-            reconnectionAttempts: Infinity,
-            reconnectionDelay: 3000 // Tenta reconectar a cada 3 segundos
+     * Function to create a module context with WebSocket, storage, and custom data capabilities.
+     * This function returns a context object with methods that allow interaction with WebSocket events, 
+     * storage, and custom data management.
+     *
+     * @param {string} moduleName - The name of the module.
+     * @returns {{
+    *   CONTEXT.MODULE_NAME: string,
+    *   SOCKET: object,
+    *   KEYBOARD_COMMANDS: Array<object>,
+    *   setStorage: (key: string, value: any, isGlobal: boolean) => Promise<object>,
+    *   getStorage: (key: string, isGlobal: boolean) => Promise<object>,
+    *   getVariable: (variableName: string, defaultValue: any, create: boolean, isGlobal: boolean) => Promise<any>,
+    *   showMenu: (options: Array<object>) => void,
+    *   getCustomData: (key: string) => any,
+    *   setCustomData: (key: string, value: any) => void
+    *   setMenuHandler: (handlerFunction: function) => void
+    * }} - The context object with methods for WebSocket, storage, and custom data.
+   */
+    function createContext(moduleName) {
+        return window.WSACTION.createModuleContext(moduleName);
+    }
+
+    // Criar o contexto para o módulo utilizando a função createModuleContext
+    const CONTEXT = createContext("CONNECTION-INFO");
+
+    const SOCKET = CONTEXT.SOCKET;
+    CONTEXT.KEYBOARD_COMMANDS = []
+
+    let swalInstance;
+
+    // Exibe um modal de "Conectando" ao iniciar a conexão
+    swalInstance = Swal.fire({
+        title: 'Conectando...',
+        text: 'Aguarde enquanto tentamos conectar ao WSActions.',
+        icon: 'info',
+        allowOutsideClick: false,
+        showConfirmButton: false
+    });
+
+    SOCKET.on('connect', () => {
+        console.log(`${CONTEXT.MODULE_NAME} Connected to WebSocket server`);
+        Swal.fire({
+            title: 'Conectado!',
+            text: 'Conexão estabelecida com o WSActions.',
+            icon: 'success',
+            timer: 2000, // Exibe por 2 segundos antes de fechar
+            showConfirmButton: false
         });
+    });
 
-        const KEYBOARD_COMMANDS = []
+    SOCKET.on('disconnect', () => {
+        console.log(`${CONTEXT.MODULE_NAME} Disconnected from WebSocket server`);
 
-        let swalInstance;
-
-        // Exibe um modal de "Conectando" ao iniciar a conexão
+        // Exibe um modal "Desconectado"
         swalInstance = Swal.fire({
-            title: 'Conectando...',
-            text: 'Aguarde enquanto tentamos conectar ao WSActions.',
-            icon: 'info',
+            title: 'Sem conexão com WSActions!',
+            text: 'Você está sem conexão com o servidor WSActions.',
+            icon: 'error',
             allowOutsideClick: false,
             showConfirmButton: false
         });
+    });
 
-        SOCKET.on('connect', () => {
-            console.log(`${MODULE_NAME} Connected to WebSocket server`);
+    SOCKET.on('reconnecting', (attempt) => {
+        console.log(`${CONTEXT.MODULE_NAME} Reconnecting to WebSocket server, attempt: ${attempt}`);
 
-            // Atualiza o modal para "Conectado"
-            Swal.fire({
-                title: 'Conectado!',
-                text: 'Conexão estabelecida com o WSActions.',
-                icon: 'success',
-                timer: 2000, // Exibe por 2 segundos antes de fechar
-                showConfirmButton: false
-            });
+        // Atualiza o modal para "Reconectando"
+        swalInstance = Swal.fire({
+            title: 'Reconectando...',
+            text: `Tentando reconectar ao WSActions (Tentativa ${attempt})`,
+            icon: 'warning',
+            allowOutsideClick: false,
+            showConfirmButton: false
         });
+    });
 
-        SOCKET.on('disconnect', () => {
-            console.log(`${MODULE_NAME} Disconnected from WebSocket server`);
+    SOCKET.on('reconnect', () => {
+        console.log(`${CONTEXT.MODULE_NAME} Reconnected to WebSocket server`);
 
-            // Exibe um modal "Desconectado"
-            swalInstance = Swal.fire({
-                title: 'Sem conexão com WSActions!',
-                text: 'Você está sem conexão com o servidor WSActions.',
-                icon: 'error',
-                allowOutsideClick: false,
-                showConfirmButton: false
-            });
+        // Atualiza o modal para "Reconectado"
+        Swal.fire({
+            title: 'Reconectado!',
+            text: 'Conexão restabelecida com sucesso ao WSActions.',
+            icon: 'success',
+            timer: 2000, // Exibe por 2 segundos antes de fechar
+            showConfirmButton: false
         });
-
-        SOCKET.on('reconnecting', (attempt) => {
-            console.log(`${MODULE_NAME} Reconnecting to WebSocket server, attempt: ${attempt}`);
-
-            // Atualiza o modal para "Reconectando"
-            swalInstance = Swal.fire({
-                title: 'Reconectando...',
-                text: `Tentando reconectar ao WSActions (Tentativa ${attempt})`,
-                icon: 'warning',
-                allowOutsideClick: false,
-                showConfirmButton: false
-            });
-        });
-
-        SOCKET.on('reconnect', () => {
-            console.log(`${MODULE_NAME} Reconnected to WebSocket server`);
-
-            // Atualiza o modal para "Reconectado"
-            Swal.fire({
-                title: 'Reconectado!',
-                text: 'Conexão restabelecida com sucesso ao WSActions.',
-                icon: 'success',
-                timer: 2000, // Exibe por 2 segundos antes de fechar
-                showConfirmButton: false
-            });
-        });
-
-        return {
-            MODULE_NAME,
-            KEYBOARD_COMMANDS,
-            SOCKET
-        };
-    }
-
-    const context = await MakeContext();
+    });
 
     // Register the extension in the global context
     if (window.extensionContext) {
-        window.extensionContext.addExtension(context.MODULE_NAME, {
+        window.extensionContext.addExtension(CONTEXT.MODULE_NAME, {
             location: window.location,
-            ...context
+            ...CONTEXT
         });
     }
 })();
